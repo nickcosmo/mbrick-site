2ffd9577d0fa3c08e627a182d8d2f3b402f2d8bc
Nick Lansing <nicklans92@gmail.com>
2021-11-14 21:34:29 -0800

    updates to contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center" align="center">
     <!-- TODO Add contact form! -->
-    <v-col cols="12" class="text-center ma-0 pa-0">
+    <v-col cols="12" class="ma-0 pa-0">
       <v-img
         max-height="700"
         width="100%"
@@ -10,15 +10,61 @@
         gradient="to bottom, rgba(0, 0, 0, 0), 10%, #121212"
         :src="require('@/static/brick-2.jpg')"
       >
-        <p class="text-md-h5 text-h6 mt-5">
-          Let's get in touch!
-        </p>
-        <a href="tel:1-800-413-4588">
-          <v-btn large rounded color="white" class="black--text my-5">
-            call now 1-800-413-4588
-          </v-btn>
-        </a>
+        <v-row>
+          <v-col class="text-center">
+            <p class="text-md-h1 text-h2 my-5">
+              LET'S TALK BRICK!
+            </p>
+          </v-col>
+        </v-row>
+        <v-row class="d-flex flex-column flex-md-row align-center justify-center">
+          <v-col class="col-md-5 col-12">
+            <v-card height="450" tile class="mb-1">
+              <v-card-title class="text-md-h4 text-h5 font-weight-bold">
+                Innovative Brick, LLC Broomfield, CO
+              </v-card-title>
+              <v-card-subtitle class="text-subtitle-1 text-md-h6">
+                Please contact us for technical asistance or questions about your next thin brick
+                project.
+              </v-card-subtitle>
+              <v-card-text>
+                <v-list>
+                  <v-list-item class="text-subtitle-1 text-md-h6">
+                    Call us now: 1-800-413-4588
+                  </v-list-item>
+                  <v-list-item class="text-subtitle-1 text-md-h6">
+                    Email: contact@mbrick.com
+                  </v-list-item>
+                  <v-list-item class="text-subtitle-1 text-md-h6">
+                    Drop by: 11625 Reed Court, Broomfield, CO, 80020
+                  </v-list-item>
+                </v-list>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col class="col-md-5 col-12 align-center text-center">
+            <iframe
+              width="600"
+              height="450"
+              style="border:0"
+              loading="lazy"
+              allowfullscreen
+              :src="mapUrl"
+            >
+            </iframe>
+          </v-col>
+        </v-row>
       </v-img>
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      mapUrl: `https://www.google.com/maps/embed/v1/place?key=${process.env.GMAPS_KEY}&q=Innovative+Brick+Systems,Broomfield+CO`
+    };
+  }
+};
+</script>
