a4596ad49a8b6c1dfea67df88620eb14e4fd75d7
Nick Lansing <nicklans92@gmail.com>
2021-11-16 21:54:48 -0800

    update tiltup project layout

--- pages/projects/tilt-up/_projectid/index.vue
+++ pages/projects/tilt-up/_projectid/index.vue
@@ -1,5 +1,27 @@
 <template>
   <v-row>
+    <v-overlay z-index="500" opacity="0.9" v-if="showCarousel">
+      <v-row>
+        <v-btn @click="showCarousel = !showCarousel" fab fixed top right dark>
+          <v-icon dark>
+            mdi-close
+          </v-icon>
+        </v-btn>
+        <v-col class="col-12" height="100%">
+          <v-carousel>
+            <v-carousel-item v-for="image in images" :key="image.id">
+              <v-img
+                class="mx-auto"
+                width="100%"
+                max-height="700"
+                max-width="700"
+                :src="image.filename"
+              ></v-img>
+            </v-carousel-item>
+          </v-carousel>
+        </v-col>
+      </v-row>
+    </v-overlay>
     <v-col cols="12" class="ma-0 pa-0">
       <v-img
         max-height="300"
@@ -8,7 +30,8 @@
           justify-center
           align-center
           text-center
-          display-4
+          text-md-h1
+          text-h2
           text-uppercase
         "
         gradient="to bottom, rgba(0, 0, 0, 0), 10%, #121212"
@@ -18,35 +41,37 @@
       </v-img>
       <v-row no-gutters class="align-center justify-center px-10 px-md-0">
         <v-col class="col-12 col-md-6">
-          <v-card class="pa-0">
-            <v-carousel :show-arrows="false">
-              <v-carousel-item v-for="image in images" :key="image.id">
-                <v-img
-                  class="mx-auto"
-                  width="100%"
-                  max-height="700"
-                  max-width="700"
-                  :src="image.filename"
-                ></v-img>
-              </v-carousel-item>
-            </v-carousel>
-            <v-card-text>
-              <v-list>
-                <v-list-item class="text-subtitle-1 text-md-h6">
+          <v-card class="pa-0" tile>
+            <v-card-text class="pb-0">
+              <v-list class="pa-0">
+                <v-list-item class="px-0 text-subtitle-1 text-md-h6">
                   {{ project.description }}
                 </v-list-item>
-                <v-list-item class="text-subtitle-1 text-md-h6">
+                <v-list-item class="px-0 text-subtitle-1 text-md-h6">
                   Location: {{ project.location }}
                 </v-list-item>
-                <v-list-item class="text-subtitle-1 text-md-h6">
+                <v-list-item class="px-0 text-subtitle-1 text-md-h6">
                   Precaster: {{ project.precaster }}
                 </v-list-item>
-                <v-list-item class="text-subtitle-1 text-md-h6">
+                <v-list-item class="px-0 text-subtitle-1 text-md-h6">
                   Completion Date: {{ project.date }}
                 </v-list-item>
               </v-list>
             </v-card-text>
           </v-card>
+        </v-col>
+      </v-row>
+      <v-row no-gutters class="align-center justify-center px-10 px-md-0">
+        <v-col class="col-12 col-md-6 d-flex flex-wrap">
+          <v-img
+            v-for="image in images"
+            :key="image.id"
+            height="352"
+            width="300"
+            :src="image.filename"
+            class="ma-md-3 ma-0"
+            @click="showCarousel = !showCarousel"
+          ></v-img>
         </v-col>
       </v-row>
     </v-col>
@@ -59,6 +84,8 @@ export default {
     if (context.store.state.projects.tiltup.length) {
       return {
         images: [],
+        imageHeight: null,
+        showCarousel: false
       };
     } else {
       context.redirect("/");
@@ -69,10 +96,10 @@ export default {
       return this.$store.getters.getSingleTiltUpProject(
         this.$route.params.projectid
       );
-    },
+    }
   },
   created() {
     this.images = this.project.images;
-  },
+  }
 };
 </script>
