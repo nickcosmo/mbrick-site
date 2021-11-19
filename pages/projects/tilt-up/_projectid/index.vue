6b57af6c3ad9b81510640a5d1b465e0bf66a8f35
Nick Lansing <nicklans92@gmail.com>
2021-11-18 21:05:12 -0800

    image styling update

--- pages/projects/tilt-up/_projectid/index.vue
+++ pages/projects/tilt-up/_projectid/index.vue
@@ -71,6 +71,7 @@
             :src="image.filename"
             class="ma-md-3 my-3"
             @click="showCarousel = !showCarousel"
+            style="cursor: pointer;"
           ></v-img>
         </v-col>
       </v-row>
