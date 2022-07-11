0ecfbc76e1098c7327124930dde859e22ff36fac
Nick Lansing <nicklans92@gmail.com>
2022-07-11 16:31:22 -0700

    footer update

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,11 +1,19 @@
 <template>
-<!-- TODO remove this margin on footer -->
   <v-footer app absolute padless class="mt-6" no-gutters>
     <v-row justify="center" no-gutters>
+      <v-col class="col-12 col-md-6 d-flex justify-center justify-md-end align-center pr-md-4">
+        <v-img
+          contain
+          height="100%"
+          max-width="160"
+          :src="require('../static/ibs-white.png')"
+        ></v-img>
+      </v-col>
+      <v-divider v-if="!isMobile" vertical class="my-4"></v-divider>
       <v-col
-        class="py-4 d-flex flex-md-row flex-column justify-space-around"
-        cols="12"
+        class="col-12 col-md-6 py-4 pl-md-4 justify-center justify-md-start align-center"
       >
+      <v-row no-gutters class="justify-center justify-md-start">
         <a href="mailto:contact@mbrick.com">
           <v-btn
             class="my-md-0 my-1 mx-0 px-3"
@@ -18,6 +26,8 @@
             <v-icon class="pr-2">mdi-email</v-icon>contact@mbrick.com
           </v-btn>
         </a>
+      </v-row>
+      <v-row no-gutters class="justify-center justify-md-start">
         <a href="tel:1-800-413-4588">
           <v-btn
             class="my-md-0 my-1 mx-0 px-3"
@@ -30,6 +40,8 @@
             <v-icon class="pr-2">mdi-phone</v-icon>1-800-413-4588
           </v-btn>
         </a>
+      </v-row>
+      <v-row no-gutters class="justify-center justify-md-start">
         <a
           href="https://www.google.com/maps/place/Innovative+Brick+Systems/@39.9084192,-105.0751496,15z/data=!4m5!3m4!1s0x0:0x7054d0219d3e7d45!8m2!3d39.9083797!4d-105.0751196"
         >
@@ -45,17 +57,22 @@
             <v-icon class="pr-2">mdi-home</v-icon>11625 Reed Court Broomfield CO
           </v-btn>
         </a>
-        <v-btn
-          class="my-md-0 my-1 mx-0 px-3"
-          :x-large="!isMobile"
-          :small="isMobile"
-          :block="isMobile"
-          text
-          rounded
-        >
-          <v-icon class="pr-2">mdi-clock</v-icon>
-          Mon - Fri: 8AM - 5PM MST
-        </v-btn>
+      </v-row>
+      <v-row no-gutters class="justify-center justify-md-start">
+        <a href="/">
+          <v-btn
+            class="my-md-0 my-1 mx-0 px-3"
+            :x-large="!isMobile"
+            :small="isMobile"
+            :block="isMobile"
+            text
+            rounded
+          >
+            <v-icon class="pr-2">mdi-clock</v-icon>
+            Mon - Fri: 8AM - 5PM MST
+          </v-btn>
+        </a>
+      </v-row>
       </v-col>
       <v-col class="py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} —
