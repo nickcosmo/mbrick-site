13bf271f82bcab959b1ebe1fc84ddbfadd4fed81
Nick Lansing <nicklans92@gmail.com>
2023-07-06 07:39:37 -0700

    updates to front page

--- pages/index.vue
+++ pages/index.vue
@@ -4,21 +4,39 @@
     <v-col cols="12" class="text-center ma-0 pa-0">
       <v-img
         width="100%"
-        class="d-flex align-center mb-10"
+        class="d-flex align-center mb-10 text-align-left"
         eager
         gradient="to bottom, rgba(0, 0, 0, 0), 10%, #121212"
         :src="require('../static/brick-hero-alt.jpg')"
       >
-        <h2 class="text-md-h3 text-sm-h4 text-h5 mt-5">INDUSTRY LEADER IN</h2>
-        <h1 class="text-md-h1 text-h2 mt-5">CUSTOM THIN-BRICK DESIGN</h1>
-        <p class="text-md-h5 text-h6 mt-5">
-          Concrete Form Liners for Embedded Brick, Tile, and Stone Applications
-        </p>
-        <a href="tel:1-800-413-4588">
-          <v-btn large rounded color="white" class="black--text my-5">
-            call now 1-800-413-4588
-          </v-btn>
-        </a>
+        <v-row justify="center" align="center">
+          <v-col class="col-5">
+            <v-img
+              height="100%"
+              width="100%"
+              :src="require('../static/ibs-white.png')"
+            ></v-img>
+          </v-col>
+          <v-col class="col-9">
+            <!-- <h1 class="text-md-h1 text-h2 mt-5 text-center">
+              INNOVATIVE BRICK SYSTEMS
+            </h1> -->
+            <h2 class="text-md-h3 text-sm-h4 text-h5 mt-5 text-center">
+              - INDUSTRY LEADER IN CUSTOM THIN-BRICK DESIGN -
+            </h2>
+          </v-col>
+          <v-col class="col-8 d-flex justify-center">
+            <p class="text-md-h5 text-h6 mt-5 text-center">
+              Concrete Form Liners for Embedded Brick, Tile, and Stone
+              Applications
+            </p>
+            <a href="tel:1-800-413-4588">
+              <v-btn large rounded color="white" class="black--text my-5">
+                call now 1-800-413-4588
+              </v-btn>
+            </a>
+          </v-col>
+        </v-row>
       </v-img>
       <v-row
         class="mt-md-12 mx-md-12 mx-3 mt-3 flex-column-reverse flex-md-row"
